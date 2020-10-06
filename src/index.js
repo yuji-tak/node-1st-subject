bb7bb271bc13f4484b44b480be8572e29fbc1c12
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-06 12:29:49 +0900

    ejsを導入

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ const express = require('express')
 const app = express()
 
 const port = 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+// const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath))
+// app.use(express.static(publicDirectoryPath))
+
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  var data = {
+    title: 'test'
+  }
+
+  res.render('./index.ejs', data)
+})
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
