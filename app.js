1e720a1ab9211abfcf64c732189771c5f766955d
yuji-tak <y.tsuaki@gmail.com>
2020-10-06 12:36:05 +0900

    不要なファイルを削除 & ファイルパスを整理

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-
 const port = 3000
-// const publicDirectoryPath = path.join(__dirname, '../public')
-
-// app.use(express.static(publicDirectoryPath))
-
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
