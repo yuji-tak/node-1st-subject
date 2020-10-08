5362cd6cde1f7da6fafce6728a2ab4ec91dd198f
yuji-tak <y.tsuaki@gmail.com>
2020-10-08 11:39:14 +0900

    signup.ejsのルーティング処理をrouter.jsへ切り出し

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const router = require('./router')
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  var data = {
-    title: 'signup'
-  }
-
-  res.render('signup.ejs', data)
-})
-
 app.use(router)
 
 app.listen(port, () => {
