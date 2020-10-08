78ffbe5b5598b0ce7f7721553c3c9b0f4c2b9407
yuji-tak <y.tsuaki@gmail.com>
2020-10-08 11:54:30 +0900

    責務の分離を目的とし、各ルーティングを分離

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const router = require('./router')
+const signupRouter = require('./routes/signup')
+const loginRouter = require('./routes/login')
 
 app.set('view engine', 'ejs')
-
-app.use(router)
+app.use(signupRouter)
+app.use(loginRouter)
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
---
+++ routes/signup.js
@@ -0,0 +1,10 @@
+const app = require('express')
+const router = new app.Router()
+
+router.get('/', (req,res) => {
+  res.render('signup', {
+    title: 'signup'
+  })
+})
+
+module.exports = router
