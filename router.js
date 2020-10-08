29372c25c278056ed793f3cc654d92592f5d40e5
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-08 11:39:14 +0900

    signup.ejsのルーティング処理をrouter.jsへ切り出し

--- router.js
+++ router.js
@@ -1,6 +1,12 @@
 const app = require('express')
 const router = app.Router()
 
+router.get('/', (req,res) => {
+  res.render('signup', {
+    title: 'signup'
+  })
+})
+
 router.get('/login', (req, res) => {
   res.render('login', {
     title: 'login'
