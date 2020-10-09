2244f1f0b599778f550e9a18232a51949d29faef
yuji-tak <y.tsuaki@gmail.com>
2020-10-09 18:04:02 +0900

    refactoring

--- routers/home.js
+++ routers/home.js
@@ -1,5 +1,5 @@
-const app = require('express')
-const router = new app.Router()
+const express = require('express')
+const router = new express.Router()
 
 router.get('/home', (req, res) => {
   res.render('home', {
