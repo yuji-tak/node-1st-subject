35b1ad9edaa5046fd485d50029c267fff8fa552f
yuji-tak <y.tsuaki@gmail.com>
2020-10-09 12:44:56 +0900

    スペースを調整

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 const app = require('express')
 const router = new app.Router()
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   res.render('signup', {
     title: 'signup'
   })
