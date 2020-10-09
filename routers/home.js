e6c3183a5ad9838371535239a1f2002cb8629894
yuji-tak <y.tsuaki@gmail.com>
2020-10-09 19:26:33 +0900

    リクエストメソッドの修正 & 入力情報の取得

--- routers/home.js
+++ routers/home.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = new express.Router()
 
 router.post('/home', (req, res) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPassword: req.body.confirmpassword
+  }
+  console.log(user)
+
   res.render('home', {
     title: 'home'
   })
--- routers/signup.js
+++ routers/signup.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('signup', {
     title: 'signup'
   })
