728209944c3c9685f439a87d53bf11597b141c7f
yuji-tak <y.tsuaki@gmail.com>
2020-10-10 17:29:02 +0900

    入力検証ロジックを移動

--- routers/home.js
+++ routers/home.js
@@ -1,29 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 
-router.post('/home', ({ body }, res) => {
-  // 入力情報の取得
-  const user = {
-    name: body.name,
-    email: body.email,
-    password: body.password,
-    confirmedPassword: body.confirmpassword
-  }
-  console.log(user)
-
-  // 入力情報の検証
-  if (user.password.length <= 7) {
-    throw new Error('7文字以下')
-  }
-
-  if (user.password !== user.confirmedPassword) {
-    throw new Error('パスワードが確認用パスワードと一致しない')
-  }
-
-  if (!user.hasOwnProperty('name', 'email', 'password', 'confirmedPassword')) {
-    throw new Error('未入力の項目がある')
-  }
-
+router.get('/home', (req, res) => {
   res.render('home', {
     title: 'home',
     name: user.name
--- routers/signup.js
+++ routers/signup.js
@@ -7,8 +7,29 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
-  console.log('post from /')
+router.post('/', ({ body }, res) => {
+  // 入力情報の取得
+  const user = {
+    name: body.name,
+    email: body.email,
+    password: body.password,
+    confirmedPassword: body.confirmpassword
+  }
+  console.log(user)
+
+  // 入力情報の検証
+  if (user.password.length <= 7) {
+    throw new Error('7文字以下')
+  }
+
+  if (user.password !== user.confirmedPassword) {
+    throw new Error('パスワードが確認用パスワードと一致しない')
+  }
+
+  if (!user.hasOwnProperty('name', 'email', 'password', 'confirmedPassword')) {
+    throw new Error('未入力の項目がある')
+  }
+
   res.render('signup', {
     title: 'signup'
   })
