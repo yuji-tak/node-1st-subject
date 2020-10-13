c1f1e03e3e8fb5cdb0ddc714366bfe73e6b1f39c
yuji-tak <y.tsuaki@gmail.com>
2020-10-13 14:49:01 +0900

    controllers/signupController.jsにて関数を切り出し 続き

--- routers/signup.js
+++ routers/signup.js
@@ -4,47 +4,6 @@ const signupController = require('../controllers/signupController')
 
 router.get('/', signupController.doGetSignup)
 
-router.post('/', ({ body }, res) => {
-  // 入力情報の取得
-  const user = {
-    name: body.name,
-    email: body.email,
-    password: body.password,
-    confirmedPassword: body.confirmpassword
-  }
-
-  // 入力情報の検証用データを作成
-  const validateUserValues = Object.values(user)
-  const isAllRequired = validateUserValues.includes('')
-
-  // 入力情報の検証
-  if (user.password.length <= 7) {
-    res.render('signup', {
-      title: 'signup',
-      errorMessagePasswordLength: 'パスワードは7文字以上で入力',
-      errorMessageConfirmedPassword: '',
-      errorMessageRequired: ''
-    })
-  } else if (user.password !== user.confirmedPassword) {
-    res.render('signup', {
-      title: 'signup',
-      errorMessagePasswordLength: '',
-      errorMessageConfirmedPassword: 'パスワードが確認用パスワードと一致しない',
-      errorMessageRequired: ''
-    })
-  } else if (isAllRequired) {
-    res.render('signup', {
-      title: 'signup',
-      errorMessagePasswordLength: '',
-      errorMessageConfirmedPassword: '',
-      errorMessageRequired: '未入力の項目がある'
-    })
-  } else {
-    res.render('home', {
-      title: 'home',
-      name: user.name
-    })
-  }
-})
+router.post('/', signupController.doPostSignup)
 
 module.exports = router
